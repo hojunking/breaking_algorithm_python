b7a5d1f2a3f6679090c7cea680b54b29e9fab89f
hojun <84840627+hojunking@users.noreply.github.com>
2022-04-08 20:37:46 +0900

    update floyd

--- dynamic/FloydAlgorithm.py
+++ dynamic/FloydAlgorithm.py
@@ -1,1 +1,14 @@
 #c++ 소스로 파이썬 할 예정!
+
+def floyd2 (n,W,D,P):
+    for i in range (1,n+1):
+        for j in range (1,n+1):
+            D[i][j] = W[i][j]
+            P[i][j] = 0
+    
+    for k in range(1,n+1):
+        for i in range(1,n+1):
+            for j in range (1,n+1):
+                if(D[i][j] > D[i][k] + D[k][j]):
+                    D[i][j] = D[i][k] + D[k][j]
+                    P[i][j] =k
