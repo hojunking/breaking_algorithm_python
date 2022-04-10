349c1f540bbaad76d8bb315c5c0616c84311692d
hojun <84840627+hojunking@users.noreply.github.com>
2022-04-10 16:42:21 +0900

    hi CMM~

---
+++ dynamic/ChainedMatrixMuliplication.py
@@ -0,0 +1,37 @@
+
+from re import I
+#chained matrix multiplication
+
+INF = 99999
+
+def minimum(i,j,d,M):
+    minValue, minK =INF, -1
+    for k in range(i,j):
+        value = M[i][k] + M[k+1][j] + d[i-1]*d[k]*d[j]
+        if minValue > value:
+            minValue, minK = value,k
+    return minValue,minK
+
+def minMult(n,d): #make matrix, chained, multiplication
+    M = [[0] * (n+1) for _ in range(n+1)] # set matrix 0
+    P = [[0] * (n+1) for _ in range(n+1)]
+    for diagonal in range (1,n): #1~n-1 input matrix 
+        for i in range (1,n-diagonal+1):
+            j = i + diagonal
+            M[i][j], P[i][j] = minimum(i,j,d,M)
+    return M,P
+
+def printMatrix(M):
+    for i in range(1, len(M)):
+        for j in range(i, len(M[i])):
+            if j < len(M[i])-1:
+                print(M[i][j], end=" ")
+            else:
+                print(M[i][j])
+
+n = int(input())
+d = list(map(int, input().split())) #default " "
+M,P = minMult(n,d)
+printMatrix(M)
+printMatrix(P)
+
